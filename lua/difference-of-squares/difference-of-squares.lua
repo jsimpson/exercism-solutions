7a9a6846a92af7fd130f723835456e0caa63d246
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-19 15:41:26 -0700

    lua: difference of squares

---
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -0,0 +1,23 @@
+local diff = {}
+
+function diff.square_of_sums(n)
+    total = 0
+    for i=1, n do
+        total = total + i
+    end
+    return total^2
+end
+
+function diff.sum_of_squares(n)
+    total = 0
+    for i=1, n do
+        total = total + (i^2)
+    end
+    return total
+end
+
+function diff.difference_of_squares(n)
+    return diff.square_of_sums(n) - diff.sum_of_squares(n)
+end
+
+return diff

efa55c071f35d655927702297ab453908028911e
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-18 23:37:30 -0700

    lua: hamming distance

---
+++ lua/hamming/hamming.lua
@@ -0,0 +1,19 @@
+local hamming = {}
+
+function hamming.compute(s1, s2)
+    if #s1 ~= #s2 then
+        return -1
+    end
+
+    local total = 0
+
+    for i=1,#s1 do
+        if s1:byte(i) ~= s2:byte(i) then
+            total = total + 1
+        end
+    end
+
+    return total
+end
+
+return hamming

d3684a4a3ccbf04dc3d321cd3aa5d35db3be00ec
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-19 09:27:32 -0700

    cleanup

--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,23 +1,14 @@
-local factors = {
-    { 3, 'Pling' },
-    { 5, 'Plang' },
-    { 7, 'Plong' }
-}
+return function (n)
+    local factors = {
+        { 3, 'Pling' },
+        { 5, 'Plang' },
+        { 7, 'Plong' }
+    }
 
-function raindrops(n)
     local ret = ''
     for _, tbl in ipairs(factors) do
         local factor, noise = unpack(tbl)
-        if n % factor == 0 then
-            ret = ret .. noise
-        end
+        if n % factor == 0 then ret = ret .. noise end
     end
-
-    if ret == '' then
-        ret = tostring(n)
-    end
-
-    return ret
+    return ret ~= '' and ret or tostring(n)
 end
-
-return raindrops

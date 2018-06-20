edf72e9e6d7924bddb8da293f07fd3d1f9fd76f0
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-20 00:16:47 -0700

    ruby: hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,13 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Hamming
+  class << self
+    def compute(s1, s2)
+      raise ArgumentError unless s1.size == s2.size
+
+      s1.chars.zip(s2.chars).count { |a, b| a!=b }
+    end
+  end
+end

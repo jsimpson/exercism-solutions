f0921c95684d42d6aecb5b90f0ebe90a47deec96
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-20 22:55:05 -0700

    ruby: difference of squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,21 @@
+module BookKeeping
+  VERSION = 1
+end
+
+class Squares
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    (1..@number).reduce(:+)**2
+  end
+
+  def sum_of_squares
+    (1..@number).inject { |sum, n| sum + n**2 }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end

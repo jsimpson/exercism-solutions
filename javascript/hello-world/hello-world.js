0af50309490077ada0f657d1c00f39bf4f5e3db0
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-22 15:37:52 -0700

    js: hello world

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,15 +1,7 @@
-//
-// This is a stub file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at hello-world.spec.js--that should give you some hints about what is
-// expected here.
-
 var HelloWorld = function () {};
 
 HelloWorld.prototype.hello = function () {
-//
-// YOUR CODE GOES HERE
-//
+  return "Hello, World!";
 };
 
 module.exports = HelloWorld;

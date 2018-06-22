584f92e187afdaf20dac25d805dac35647482fae
Jonathan Simpson <jsimpson.github@gmail.com>
2018-06-21 22:31:57 -0700

    cleanup

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -2,16 +2,13 @@ class Prime
   class << self
     def nth(n)
       raise ArgumentError if n == 0
-
       primes = []
-
       i = 2
       loop do
         primes << i if is_prime?(primes, i)
         break if primes.size >= n
         i += 1
       end
-
       primes.last
     end
 
